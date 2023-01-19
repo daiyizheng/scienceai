1f07d37023dc6c3b6f5444a324d6acd93a49869d
daiyizheng <387942239@qq.com>
2023-01-19 15:09:32 +0800

    init

---
+++ scripts/graph.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+'''
+Filename         :graph.py
+Description      :
+Time             :2023/01/07 18:41:47
+Author           :daiyizheng
+Email            :387942239@qq.com
+Version          :1.0
+'''
+
+class Graph(object):
+    def __init__(self) -> None:
+        pass
